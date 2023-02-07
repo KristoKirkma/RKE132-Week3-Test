9095ba47fe7b57d1744ec867ecf13242317c2321
Kristo <kristo.kirkma@tktk.ee>
2023-02-07 10:26:16 +0200

    add week3 test

---
+++ If/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("Mis tulemuse said?");
+string userGrade = Console.ReadLine().ToUpper();
+
+if (userGrade == "A")
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (userGrade == "B")
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (userGrade == "C")
+{
+    Console.WriteLine("Hea!");
+}
+else if (userGrade == "D")
+{
+    Console.WriteLine("Rahuldav");
+}
+else if (userGrade == "E")
+{
+    Console.WriteLine("Kasin");
+}  
+else if (userGrade == "F")
+{
+    Console.WriteLine("Puudulik");
+}
+else
+{
+    Console.WriteLine("Vale väärtus");
+}
